5bc38d3a9929ccf7c5c765f65b239c20ed30bc56
Tajamul Arafath VP <tajamul.arafath@colanonline.com>
2026-06-05 16:38:02 +0530

    Added: Working on Steams API

--- src/collections/set/treeset/comparator/Employee.java
+++ src/collections/set/treeset/comparator/Employee.java
@@ -19,4 +19,28 @@ public class Employee {
     public String toString() {
         return this.id+" "+ this.name+ " "+ this.salary;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
--- src/java8/streamAPI/RemoveDuplicates.java
+++ src/java8/streamAPI/RemoveDuplicates.java
@@ -1,8 +1,11 @@
 package java8.streamAPI;
 
-import java.util.Arrays;
-import java.util.List;
+import collections.set.treeset.comparator.Employee;
+
+import java.security.Key;
+import java.util.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
@@ -19,5 +22,44 @@ public class RemoveDuplicates {
                 .distinct()
                 .collect(Collectors.toList());
         System.out.println("EvenNumbers "+ evenNum);
+
+
+        /// Removing Duplicate using filter method
+        List<Integer> numList = Arrays.asList(10,100,20,101,99,4,2,4,6,1);
+        Set<Integer> set = new LinkedHashSet<>();
+        List<Integer> sortedList = numList.stream()
+                .filter(set::add)
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println("sortedList "+sortedList);
+
+        ///  Convert Names to Uppercase
+        List<String> names = Arrays.asList("colan", "infotech", "chennai");
+
+        List<String> upperCase = names.stream()
+                .map(String::toUpperCase)
+                .toList();
+        System.out.println("upperCase "+upperCase);
+
+        ///  Count Names Starting with "A"
+        List<String> strName = Arrays.asList("Aman", "Rahul", "Ajay", "Ankit");
+
+        List<String> countString = strName.stream()
+                .filter(k -> k.startsWith("A"))
+                .toList();
+        System.out.println("countString "+countString);
+
+
+    /// Group Employees by Department
+    List<Employee> employees = Arrays.asList(
+            new Employee(1,"Umar",2000),
+            new Employee(2,"Ali",2500),
+            new Employee(5,"Abu",2000)
+    );
+    Map<Double, List<Employee>> groupEmp = employees.stream()
+            .collect(Collectors.groupingBy(Employee::getSalary));
+
+        System.out.println("Group Employees  "+groupEmp);
+
     }
 }
